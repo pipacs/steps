03d5d5efaaf8141bc34e8b78e75a562f770e1d0c
Akos Polster <akos@pipacs.com>
2011-11-29 23:11:42 +0100

    Authenticate with Google.

--- googledocs.cpp
+++ googledocs.cpp
@@ -5,6 +5,16 @@
 
 #include "googledocs.h"
 
+/* Twitter demo keys:
+static const char *OAUTH_CONSUMER_KEY = "9PqhX2sX7DlmjNJ5j2Q";
+static const char *OAUTH_CONSUMER_SECRET_KEY = "1NYYhpIw1fXItywS9Bw6gGRmkRyF9zB54UXkTGcI8";
+static const char *OAUTH_REQUEST_TOKEN_URL = "https://api.twitter.com/oauth/request_token";
+static const char *OAUTH_GET_ACCESS_URL = "https://api.twitter.com/oauth/access_token";
+*/
+static const char *OAUTH_CONSUMER_KEY = "903309545755.apps.googleusercontent.com";
+static const char *OAUTH_CONSUMER_SECRET_KEY = "bjFH7kt7nL9jrE4t8L_x7O6W";
+static const char *OAUTH_SCOPE = "https://www.googleapis.com/auth/fusiontables";
+
 static GoogleDocs *instance_;
 
 GoogleDocs *GoogleDocs::instance() {
@@ -48,9 +58,14 @@ void GoogleDocs::link() {
     connect(oauthManager, SIGNAL(requestReady(QByteArray)), this, SLOT(onRequestReady(QByteArray)));
     connect(oauthManager, SIGNAL(openUrl(QString)), this, SIGNAL(openUrl(QString)));
 
-    oauthRequest->initRequest(KQOAuthRequest::TemporaryCredentials, QUrl("https://api.twitter.com/oauth/request_token"));
-    oauthRequest->setConsumerKey("9PqhX2sX7DlmjNJ5j2Q");
-    oauthRequest->setConsumerSecretKey("1NYYhpIw1fXItywS9Bw6gGRmkRyF9zB54UXkTGcI8");
+    // oauthRequest->initRequest(KQOAuthRequest::TemporaryCredentials, QUrl("https://api.twitter.com/oauth/request_token"));
+    oauthRequest->initRequest(KQOAuthRequest::TemporaryCredentials, QUrl("https://www.google.com/accounts/OAuthGetRequestToken"));
+    oauthRequest->setConsumerKey(OAUTH_CONSUMER_KEY);
+    oauthRequest->setConsumerSecretKey(OAUTH_CONSUMER_SECRET_KEY);
+    KQOAuthParameters parameters;
+    parameters.insert("scope", OAUTH_SCOPE);
+    parameters.insert("oauth_signature_method", "HMAC-SHA");
+    oauthRequest->setAdditionalParameters(parameters);
     oauthManager->setHandleUserAuthorization(true);
     oauthManager->executeRequest(oauthRequest);
 }
@@ -63,7 +78,8 @@ void GoogleDocs::unlink() {
 
 void GoogleDocs::onAuthorizationReceived(QString token, QString verifier) {
     qDebug() << "GoogleDocs::onAuthorizationReceived: User authorization received: " << token << verifier;
-    oauthManager->getUserAccessTokens(QUrl("https://api.twitter.com/oauth/access_token"));
+    // oauthManager->getUserAccessTokens(QUrl("https://api.twitter.com/oauth/access_token"));
+    oauthManager->getUserAccessTokens(QUrl("https://www.google.com//accounts/OAuthGetAccessToken"));
     if (oauthManager->lastError() != KQOAuthManager::NoError) {
         emit linkingFailed("oops");
     }
@@ -73,9 +89,9 @@ void GoogleDocs::onAccessTokenReceived(QString token, QString tokenSecret) {
     qDebug() << "GoogleDocs::onAccessTokenReceived: Access token received: " << token << tokenSecret;
     oauthSettings.setValue("oauth_token", token);
     oauthSettings.setValue("oauth_token_secret", tokenSecret);
+    qDebug() << " Access tokens now stored. Authentication complete.";
     emit linkedChanged();
     emit linkingSucceeded();
-    qDebug() << " Access tokens now stored. You are ready to send Tweets from user's account!";
 }
 
 void GoogleDocs::onAuthorizedRequestDone() {
@@ -89,7 +105,8 @@ void GoogleDocs::onRequestReady(QByteArray response) {
 void GoogleDocs::onTemporaryTokenReceived(QString token, QString tokenSecret) {
     qDebug() << "GoogleDocs::onTemporaryTokenReceived: Temporary token received: " << token << tokenSecret;
 
-    QUrl userAuthURL("https://api.twitter.com/oauth/authorize");
+    // QUrl userAuthURL("https://api.twitter.com/oauth/authorize");
+    QUrl userAuthURL("https://accounts.google.com/OAuthAuthorizeToken");
 
     if (oauthManager->lastError() == KQOAuthManager::NoError) {
         qDebug() << " Asking for user's permission to access protected resources. Opening URL: " << userAuthURL;
