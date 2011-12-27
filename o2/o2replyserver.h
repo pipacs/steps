fb708b6e5e79b32539892481bd2dcd4c3cdd866b
Akos Polster <akos@pipacs.com>
2011-12-27 17:17:32 +0100

    Beautify.

--- o2/o2replyserver.h
+++ o2/o2replyserver.h
@@ -3,8 +3,7 @@ O2: OAuth 2.0 authenticator for Qt.
 
 Author: Akos Polster (akos@pipacs.com). Inspired by KQOAuth, the OAuth library made by Johan Paul (johan.paul@d-pointer.com).
 
-O2 is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+O2 is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
 O2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
 
