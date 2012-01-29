d5beb5c3c4b232e5358f309820d0a70e409d1f81
Akos Polster <akos@pipacs.com>
2012-01-29 10:15:49 +0100

    Update Hungarian translations.

--- translations/generic.ts
+++ translations/generic.ts
@@ -4,27 +4,27 @@
 <context>
     <name>ActionsPage</name>
     <message>
-        <location filename="ActionsPage.qml" line="46"/>
+        <location filename="../qml/ActionsPage.qml" line="46"/>
         <source>Reset step count</source>
         <translation type="unfinished">Lépésszám nullázás</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="56"/>
+        <location filename="../qml/ActionsPage.qml" line="56"/>
         <source>Settings</source>
         <translation type="unfinished">Beállítások</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="67"/>
+        <location filename="../qml/ActionsPage.qml" line="67"/>
         <source>Reset current activity step count?</source>
         <translation type="unfinished">Nullázza a tevékenység lépésszámát?</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="79"/>
+        <location filename="../qml/ActionsPage.qml" line="79"/>
         <source>Change activity to </source>
         <translation type="unfinished">Legyen </translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="79"/>
+        <location filename="../qml/ActionsPage.qml" line="79"/>
         <source>?</source>
         <translation type="unfinished"> az új tevékenység?</translation>
     </message>
@@ -32,13 +32,23 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="MainPage.qml" line="56"/>
-        <location filename="MainPage.qml" line="108"/>
+        <location filename="../qml/MainPage.qml" line="39"/>
+        <source>Today: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="47"/>
+        <source>Total: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="56"/>
+        <location filename="../qml/MainPage.qml" line="108"/>
         <source> (Paused)</source>
         <translation type="unfinished"> (szünet)</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="67"/>
+        <location filename="../qml/MainPage.qml" line="67"/>
         <source>Press Volume Up to start/pause activity, Volume Down to show more options</source>
         <translation type="unfinished">A &quot;hangerő fel&quot; gombbal a számláló indul, a &quot;hangerő le&quot; gombbal további lehetőségek közül választhat</translation>
     </message>
@@ -46,67 +56,67 @@
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="SettingsPage.qml" line="24"/>
+        <location filename="../qml/SettingsPage.qml" line="25"/>
         <source>Conserve power</source>
         <translation type="unfinished">Energia megtakarítás</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="30"/>
+        <location filename="../qml/SettingsPage.qml" line="31"/>
         <source>Sound effects</source>
         <translation type="unfinished">Hang effektusok</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="36"/>
+        <location filename="../qml/SettingsPage.qml" line="37"/>
         <source>Show Exit icon</source>
         <translation type="unfinished">&quot;Kilépés&quot; ikon látható</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="40"/>
+        <location filename="../qml/SettingsPage.qml" line="41"/>
         <source>Sensitivity:</source>
         <translation type="unfinished">Érzékenység:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="52"/>
+        <location filename="../qml/SettingsPage.qml" line="53"/>
         <source>Save to Google Docs:</source>
         <translation type="unfinished">Mentés Google Docs-ra</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="55"/>
+        <location filename="../qml/SettingsPage.qml" line="56"/>
         <source>Enable saving</source>
         <translation type="unfinished">Mentés engedélyezve</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="62"/>
+        <location filename="../qml/SettingsPage.qml" line="63"/>
         <source>Logout from Google</source>
         <translation type="unfinished">Kilép a Google Docs-ból</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="62"/>
+        <location filename="../qml/SettingsPage.qml" line="63"/>
         <source>Login to Google</source>
         <translation type="unfinished">Belép a Google Docs-ba</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="74"/>
+        <location filename="../qml/SettingsPage.qml" line="76"/>
         <source>Rename activity &quot;Custom 1&quot;:</source>
         <translation type="unfinished">&quot;Saját 1&quot; tevékenység új neve:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="82"/>
+        <location filename="../qml/SettingsPage.qml" line="88"/>
         <source>Rename activity &quot;Custom 2&quot;:</source>
         <translation type="unfinished">&quot;Saját 2&quot; tevékenység új neve:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="94"/>
+        <location filename="../qml/SettingsPage.qml" line="135"/>
         <source>Are you sure to log out?</source>
         <translation type="unfinished">Biztosan kilép?</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="110"/>
+        <location filename="../qml/SettingsPage.qml" line="151"/>
         <source>Logged in to Google Docs</source>
         <translation type="unfinished">Belépett a Google Docs-ba</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="110"/>
+        <location filename="../qml/SettingsPage.qml" line="151"/>
         <source>Logged out from Google Docs</source>
         <translation type="unfinished">Kilépett a Google Docs-ból</translation>
     </message>
@@ -114,22 +124,22 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Walking</source>
         <translation type="unfinished">Séta</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Running</source>
         <translation type="unfinished">Futás</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Custom 1</source>
         <translation type="unfinished">Saját 1</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Custom 2</source>
         <translation type="unfinished">Saját 2</translation>
     </message>
--- translations/hu.ts
+++ translations/hu.ts
@@ -4,27 +4,27 @@
 <context>
     <name>ActionsPage</name>
     <message>
-        <location filename="ActionsPage.qml" line="46"/>
+        <location filename="../qml/ActionsPage.qml" line="46"/>
         <source>Reset step count</source>
         <translation>Lépésszám nullázás</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="56"/>
+        <location filename="../qml/ActionsPage.qml" line="56"/>
         <source>Settings</source>
         <translation>Beállítások</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="67"/>
+        <location filename="../qml/ActionsPage.qml" line="67"/>
         <source>Reset current activity step count?</source>
         <translation>Nullázza a tevékenység lépésszámát?</translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="79"/>
+        <location filename="../qml/ActionsPage.qml" line="79"/>
         <source>Change activity to </source>
         <translation>Legyen </translation>
     </message>
     <message>
-        <location filename="ActionsPage.qml" line="79"/>
+        <location filename="../qml/ActionsPage.qml" line="79"/>
         <source>?</source>
         <translation> az új tevékenység?</translation>
     </message>
@@ -32,13 +32,23 @@
 <context>
     <name>MainPage</name>
     <message>
-        <location filename="MainPage.qml" line="56"/>
-        <location filename="MainPage.qml" line="108"/>
+        <location filename="../qml/MainPage.qml" line="39"/>
+        <source>Today: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="47"/>
+        <source>Total: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../qml/MainPage.qml" line="56"/>
+        <location filename="../qml/MainPage.qml" line="108"/>
         <source> (Paused)</source>
         <translation> (szünet)</translation>
     </message>
     <message>
-        <location filename="MainPage.qml" line="67"/>
+        <location filename="../qml/MainPage.qml" line="67"/>
         <source>Press Volume Up to start/pause activity, Volume Down to show more options</source>
         <translation>A &quot;hangerő fel&quot; gombbal a számláló indul, a &quot;hangerő le&quot; gombbal további lehetőségek közül választhat</translation>
     </message>
@@ -46,67 +56,67 @@
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="SettingsPage.qml" line="24"/>
+        <location filename="../qml/SettingsPage.qml" line="25"/>
         <source>Conserve power</source>
         <translation>Energia megtakarítás</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="30"/>
+        <location filename="../qml/SettingsPage.qml" line="31"/>
         <source>Sound effects</source>
         <translation>Hang effektusok</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="36"/>
+        <location filename="../qml/SettingsPage.qml" line="37"/>
         <source>Show Exit icon</source>
         <translation>&quot;Kilépés&quot; ikon látható</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="40"/>
+        <location filename="../qml/SettingsPage.qml" line="41"/>
         <source>Sensitivity:</source>
         <translation>Érzékenység:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="52"/>
+        <location filename="../qml/SettingsPage.qml" line="53"/>
         <source>Save to Google Docs:</source>
         <translation>Mentés Google Docs-ra</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="55"/>
+        <location filename="../qml/SettingsPage.qml" line="56"/>
         <source>Enable saving</source>
         <translation>Mentés engedélyezve</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="62"/>
+        <location filename="../qml/SettingsPage.qml" line="63"/>
         <source>Logout from Google</source>
         <translation>Kilép a Google Docs-ból</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="62"/>
+        <location filename="../qml/SettingsPage.qml" line="63"/>
         <source>Login to Google</source>
         <translation>Belép a Google Docs-ba</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="74"/>
+        <location filename="../qml/SettingsPage.qml" line="76"/>
         <source>Rename activity &quot;Custom 1&quot;:</source>
         <translation>&quot;Saját 1&quot; tevékenység új neve:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="82"/>
+        <location filename="../qml/SettingsPage.qml" line="88"/>
         <source>Rename activity &quot;Custom 2&quot;:</source>
         <translation>&quot;Saját 2&quot; tevékenység új neve:</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="94"/>
+        <location filename="../qml/SettingsPage.qml" line="135"/>
         <source>Are you sure to log out?</source>
         <translation>Biztosan kilép?</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="110"/>
+        <location filename="../qml/SettingsPage.qml" line="151"/>
         <source>Logged in to Google Docs</source>
         <translation>Belépett a Google Docs-ba</translation>
     </message>
     <message>
-        <location filename="SettingsPage.qml" line="110"/>
+        <location filename="../qml/SettingsPage.qml" line="151"/>
         <source>Logged out from Google Docs</source>
         <translation>Kilépett a Google Docs-ból</translation>
     </message>
@@ -114,22 +124,22 @@
 <context>
     <name>main</name>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Walking</source>
         <translation>Séta</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Running</source>
         <translation>Futás</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Custom 1</source>
         <translation>Saját 1</translation>
     </message>
     <message>
-        <location filename="main.qml" line="12"/>
+        <location filename="../qml/main.qml" line="12"/>
         <source>Custom 2</source>
         <translation>Saját 2</translation>
     </message>
