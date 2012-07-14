0fae4ba0a6cfae7d713684d997e3941cd1df3fe4
Akos Polster <akos@pipacs.com>
2012-07-14 11:11:15 +0200

    Initial version.

---
+++ qcsecretexample.h
@@ -0,0 +1,11 @@
+#ifndef QC_SECRET_EXAMPLE_H
+#define QC_SECRET_EXAMPLE_H
+
+#define QC_OAUTH_CLIENT_ID "client_id"
+#define QC_OAUTH_CLIENT_SECRET "client_secret"
+#define QC_REQUEST_TOKEN_URL ""
+#define QC_AUTHORIZE_URL ""
+#define QC_ACCESS_TOKEN_URL ""
+#define QC_INPUT_URL ""
+
+#endif // QC_SECRET_EXAMPLE_H
